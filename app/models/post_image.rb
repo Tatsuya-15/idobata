baf263b060f829c5cd9be366fc8f2575ab4dd382
Tatsuya-15 <fiuto54gkar@outlook.com>
2022-02-12 10:55:19 +0000

    投稿フォームバリデーション追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,8 @@ class PostImage < ApplicationRecord
   belongs_to :admin
   attachment :image
 
+  validates :image_id, presence: true
+  validates :title, presence: true
+  validates :caption, presence: true
+
 end
